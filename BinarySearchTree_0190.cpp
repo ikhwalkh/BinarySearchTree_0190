784c17ec097b8e499b608d7b230f9c1ab887064d
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-06-12 15:47:48 +0700

    deklarasi object x sebagai object BinaryTree dan perulangan while selama bernilai benar

--- BinarySearchTree_0190.cpp
+++ BinarySearchTree_0190.cpp
@@ -130,5 +130,22 @@ public:
 
 int main()
 {
-    BinaryTree 
+    //deklarasi object x sebagai object BinaryTree
+    BinaryTree x;
+
+    //perulangan while selama bernilai benar
+    while(true)
+    {
+        //membuat menu program dan input pilihan
+        cout << "\nMenu" << endl;
+        cout << "1. implement insert operation" << endl;
+        cout << "2. perform inorder traversal" << endl;
+        cout << "3. perform preorder traversal" << endl;
+        cout << "4. perform postorder traversal" << endl;
+        cout << "5. exit" << endl;
+        cout << "\nEnter your choice (1-5): ";
+        char ch;
+        cin >> ch;
+        cout << endl;
+    }
 }
