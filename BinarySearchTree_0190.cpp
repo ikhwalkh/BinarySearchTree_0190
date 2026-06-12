941f9b51666e44e7447ee8fad19e61902668e2c2
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-06-12 15:52:43 +0700

    membuat conditional statement dari input ch

--- BinarySearchTree_0190.cpp
+++ BinarySearchTree_0190.cpp
@@ -144,8 +144,15 @@ int main()
         cout << "4. perform postorder traversal" << endl;
         cout << "5. exit" << endl;
         cout << "\nEnter your choice (1-5): ";
+
         char ch;
         cin >> ch;
         cout << endl;
+
+        //membuat conditional statement dari input ch
+        switch (ch)
+        {
+            case '1':
+        }
     }
 }
