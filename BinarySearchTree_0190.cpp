870ded7f71e07b2b91a460c02ff0b49b6a7e9449
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-06-12 15:23:24 +0700

    membuat conditional statement

--- BinarySearchTree_0190.cpp
+++ BinarySearchTree_0190.cpp
@@ -44,5 +44,13 @@ public:
     Node* parent = nullptr;
     Node* currentNode = nullptr;
     search(element, parent, currentNode);
+
+    //membuat conditional statement
+    if (parent == nullptr) {
+        ROOT = newNode;
+        return;
+    }
+
+    if (element < parent->info)
    }
 }
