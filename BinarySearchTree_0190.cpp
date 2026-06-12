4d827421ed14d91d8e25dd9f42d15ad67f266965
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-06-12 15:09:07 +0700

    membuat constructor untuk class node dan memberi nilai parameter ke variabel

--- BinarySearchTree_0190.cpp
+++ BinarySearchTree_0190.cpp
@@ -9,5 +9,13 @@ public:
     string info;
     Node* leftChild;
     Node* rightChild;
-    
-}
+
+
+    // membuat constructor untuk class node
+    Node(string i, Node* l, Node* r){
+        //memberi nilai parameter ke variabel
+        info = i;
+        leftChild = l;
+        rightChild = r;
+    }
+};
